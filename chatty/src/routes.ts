df4d38e10a3e6895ac5e3f6c294e9e1d6260b29e
Henrique Dantas <paulo.henrique@sapienstec.com>
2021-08-18 20:03:10 -0300

    rota de post para users

--- chatty/src/routes.ts
+++ chatty/src/routes.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import { SettingsControllers } from './controllers/SettingsControllers'
+import { UsersController } from './controllers/UsersController';
 
 
 const routes = Router();
-
+const usersControllers = new UsersController();
 const settingsControllers = new SettingsControllers();
 
-routes.post("/settings" , settingsControllers.create)
+routes.post("/users", usersControllers.create);
+routes.post("/settings" , settingsControllers.create);
 
 
 export { routes }
