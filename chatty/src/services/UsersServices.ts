010a6d5b0cc844a38b4bd1b68c4e33d9d8e03376
Henrique Dantas <paulo.henrique@sapienstec.com>
2021-08-18 19:46:05 -0300

    users services

---
+++ chatty/src/services/UsersServices.ts
@@ -0,0 +1,34 @@
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepositorie";
+
+
+class UsersServices {
+  async create(email:string) {
+    const usersRepository = getCustomRepository(UsersRepository);
+
+    //verificar se o usuario existe
+
+    const alreadyExistUser = await usersRepository.findOne({
+      email,
+    })
+
+    //se existir, retornar o user
+
+    if(alreadyExistUser) {
+      return alreadyExistUser
+    }
+
+    //se não existir eu devo criar e salvar no DB
+
+    const user = usersRepository.create({
+      email,
+    })
+
+    await usersRepository.save(user)
+
+    return user
+  }
+
+}
+
+export {UsersServices}
