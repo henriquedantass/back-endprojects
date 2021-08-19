38aad382433661a30030463eabca26918823475c
Henrique Dantas <paulo.henrique@sapienstec.com>
2021-08-18 22:51:29 -0300

    message repositorie

---
+++ chatty/src/repositories/MessagesRepositorie.ts
@@ -0,0 +1,13 @@
+import { EntityRepository, Repository } from "typeorm";
+import { Messages } from '../entities/Messages'
+
+
+@EntityRepository(Messages)
+class MessagesRepository extends Repository<Messages> {
+
+
+
+}
+
+
+export {MessagesRepository}
