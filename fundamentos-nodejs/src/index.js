847905603f663b07821862b2005cbb1f678ad95e
Henrique <henrique@hotmail.ph>
2021-12-07 15:38:25 -0300

    [UPDATE] aprendendo os metodos HTTP

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -3,8 +3,33 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.send("Hello World With Nodemon");
+/**
+ * METODOS DE REQUESTS
+ * GET -- buscar informações dentro do servidor
+ * POST -- inserir uma informação dentro do servidor
+ * PUT -- alterar uma informação do servidor
+ * PATCH -- alterar uma informação especifica no servidor
+ * DELETE -- deletar uma informação do servidor
+ */
+
+app.get("/items", (request, response) => {
+  return response.json(["item 1", "item 2", "item 3"]);
+});
+
+app.post("/items", (request, response) => {
+  return response.json(["item 1", "item 2", "item 3", "item 4"]);
+});
+
+app.put("/items/:id", (request, response) => {
+  return response.json(["item 6", "item 2", "item 3", "item 4"]);
+});
+
+app.patch("items/:id", (request, response) => {
+  return response.json(["item 6", "item 2", "item 3", "item 4"]);
+});
+
+app.delete("items/:id", (request, response) => {
+  return response.json(["item 6", "item 2", "item 4"]);
 });
 
 // A aplicação necessita de uma porta para ser ouvida
