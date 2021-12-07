85c668e560adba65da12f7d47b30f0bacded4079
Henrique <henrique@hotmail.ph>
2021-12-07 16:18:49 -0300

    [UPDATE] rota para criaçao de usuario

--- finapi/src/index.js
+++ finapi/src/index.js
@@ -1,5 +1,24 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
+app.use(express.json());
+
+const costumers = [];
+
 app.listen(3333);
+
+app.post("/account", (request, response) => {
+  const { cpf, name } = request.body;
+  const id = uuidv4();
+
+  costumers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return response.status(201).send("Usuário criado com sucesso!");
+});
