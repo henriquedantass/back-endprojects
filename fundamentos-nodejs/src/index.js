65d51589b482ec806a421c29e30afd02798107a2
Henrique <henrique@hotmail.ph>
2021-12-07 15:21:16 -0300

    [UPDATE] add nodemon and dev script

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (request, response) => {
-  return response.send("Hello World");
+  return response.send("Hello World With Nodemon");
 });
 
 // A aplicação necessita de uma porta para ser ouvida
